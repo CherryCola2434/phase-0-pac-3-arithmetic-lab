a36cc3a0bb177b56f86114c8e02333df9edaf12e
Cynthia Lopez <sleepyroad2005@gmail.com>
2023-08-19 18:03:20 -0500

    done

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function makeInt(number= 10) {
+  return parseInt(number, 10);
+  }
+function preserveDecimal(number){
+  return parseFloat(number);
+}
+function increment(number= 5) {
+  return number += 1;
+}
+function decrement(number= 5) {
+  return number -= 1;
+}
+
+
+
+
+
